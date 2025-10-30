e2ac3900a8c4b53a9e5c6a33232c5677d519088f
kakarot-spec <evan08acharya@gmail.com>
2025-10-30 12:31:10 +0545

    Simplify alert message in popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
 window.onload = function() {
-  alert("thank you for visiting my web. And i've been working on new web that will inclue all project in the future and this as well ");
+  alert("thank you for visiting my web.");
 };
 
